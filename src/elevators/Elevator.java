b98e1ff4c5f2ed1d8991792fa0ecb512bc5a7e89
bigTempArray <abdul.kazal@gmail.com>
2023-02-17 13:34:09 -0500

    Implemented engine class

--- src/elevators/Elevator.java
+++ src/elevators/Elevator.java
@@ -17,8 +17,54 @@ public class Elevator implements Runnable{
     private boolean buttons[]; // true means pressed, false otherwise
     private boolean lamps[];  // true means lights on, false otherwise
     private boolean doors;   // true is open, false is closed
-    private Object engine = new Object(); // Placeholder data type for engine
+    private Object motor = new Object(); // Placeholder data type for engine
     private Scheduler scheduler; // elevator's communication line with the scheduler
+    private State elevatorState; // Contains the current state of the elevator in state machine fashion
+    
+    /**
+     * Collection of all possible elevator states 
+     * as enumerations. Each state is as described:
+     * 
+     * standBy: 
+     * The initial state of the class. This is when there
+     * is no active objective for the elevator, thus is will remain 
+     * on whatever floor it's in with the doors indefinitely opened. 
+     * Goes into standby after finishing all requests. 
+     * 
+     * traversing:
+     * This state is active when the elevators speed is not 
+     * zero. Whether up or down, retrieving passengers or 
+     * delivering them, the state of the elevator being in 
+     * motion is denoted by these states. 
+     * 
+     * loading: 
+     * When the elevator reaches a floor and is accepting
+     * passengers to enter the car, this is the state that 
+     * the elevator will be in.
+     * 
+     * unLoading:
+     * When an elevator reaches a floor desired by the passenger,
+     * and now the passengers are exiting the car. (Even in the real 
+     * world, if there are passengers exiting and entering on a single 
+     * floor, this usually happens sequentially and not concurrently. 
+     * 
+     * stopped:
+     * An intermediary state that occur in between larger operations. 
+     * This is whenever the elevator is not moving or loading, but 
+     * not in standBy as well. This could be due to multiple causes
+     * such as before / after closing doors. Right before and after 
+     * traversal to a destination, an emergency seizure in the midst
+     * of operations, etc. 
+     * 
+     */
+    enum State {
+    	standBy,
+    	traversingUp,
+    	traversingDown,
+    	loading,
+    	unLoading,
+    	stopped
+    }
 
     //Basic getters for some essential elevator attributes
     public int getNumberOfFloors() {
@@ -31,6 +77,10 @@ public class Elevator implements Runnable{
 
     public boolean doorStatus() {
         return doors;
+    }
+    
+    public State getState() {
+    	return elevatorState;
     }
 
     /**
@@ -48,6 +98,7 @@ public class Elevator implements Runnable{
         buttons = new boolean[numberOfFloors];
         lamps = new boolean[numberOfFloors];
         doors = true;
+        elevatorState = State.standBy;
     }
 
     /**
@@ -62,20 +113,18 @@ public class Elevator implements Runnable{
         //scheduler.sendLamps(boolean lamps);
     	while (true) {
     		Passenger person = scheduler.getNextRequest();
+    		//Once the request is taken all the related information
+    		// of the destination is taken from the passenger class
+    		
     		int buttonPressed = person.getCarButton();
     		
             //Simulating button press on class creation
             buttons[buttonPressed-1] = true;
             lamps[buttonPressed-1] = true;
             
-            // scheduler.sendLamps(lamps);
+            // Get's to target floor then gives update to the scheduler
             scheduler.sendElevatorUpdates(buttonPressed);
             
     	}
     }
-    
-    
-    
-    
-    
 }
---
+++ src/elevators/Engine.java
@@ -0,0 +1,106 @@
+package elevators;
+import java.lang.Math;
+import java.util.Random;
+
+/**
+ * Engine class which represents the
+ * motor and all the accompanying
+ * Phenomena of an elevator motor.
+ * Primarily the delay of moving between
+ * floors. 
+ *
+ * @author Abdul Kazal
+ */
+public class Engine {
+	
+	double firstFloorMedianDelay;
+	double firstFloorStd;
+	double topSpeedMedianDelay;
+	double topSpeedStd;
+	double rampingUpDelay;
+	int maxSpeedDelay;
+	Random rng = new Random();
+	
+	/**
+	 * The constructor that specifies all the 
+	 * specs of the engine's ability. The unit for all
+	 * double parameters is in seconds and the byte parameter
+	 * is in the unit of floors. 
+	 * @param firstFloorD Median time to travel first floor. 
+	 * @param firstFloorS Standard deviation of firstFloorD median. 
+	 * @param maxDelay The number of floors it takes for the elevator to reach top speed.
+	 * @param topSpeed Median time to travel when engine at top speed.
+	 * @param topSpeedD Standard deviation for topSpeed.
+	 * @Param rampUpD The median time it takes to travel between floors between start and max speed
+	 * 
+	 * Note: 'D' stands for delay and 'S' stands for standard deviation.
+	 */
+	public Engine(double firstFloorD, double firstFloorS,
+			int maxDelay, double topSpeed, double topSpeedD, double rampUpD) {
+		firstFloorMedianDelay = firstFloorD;
+		firstFloorStd = firstFloorS;
+		maxSpeedDelay = maxDelay;
+		topSpeedMedianDelay = topSpeed;
+		topSpeedStd = topSpeedD;
+		rampingUpDelay = rampUpD;
+	}
+	
+	/**
+	 * Method to add the real time delay of
+	 * an elevator car traveling throughout 
+	 * the building. 
+	 * 
+	 * @param startFloor which floor the car starts moving from
+	 * @param destinationFloor the destination floor of the car
+	 * 
+	 * @return The value in seconds of the delay. (To be handled by elevator)
+	 */
+	public double traverseFloors(int startFloor, int destinationFloor) {
+		double delayInSeconds = 0;
+	
+		//Getting difference 
+		int floorGap = Math.abs(startFloor - destinationFloor);
+
+		//Elevator will not hit top speed during this trip
+		//First floor delay 
+		delayInSeconds += generateDelay(firstFloorMedianDelay, firstFloorStd);
+		//Speeding up floors delay
+		for (int floor = 0; floor < maxSpeedDelay-1; floor++) { // -1 to account for the first floor delay since already acoounted for
+			delayInSeconds += generateDelay(rampingUpDelay, 0); //Did not add standard deviation to acceleration factor due to complexity
+		}
+		
+		//Returning if max speed cannot be achieved 
+		if (floorGap - maxSpeedDelay <= 0 ) {
+			return delayInSeconds;
+		}
+		
+		//Max speed floors delay
+		for (int floor = 0; floor < floorGap - maxSpeedDelay; floor++) {
+			delayInSeconds += generateDelay(topSpeedMedianDelay, topSpeedStd);
+		}
+		return delayInSeconds;
+	}
+	
+	//Helper functions 
+	
+	/**
+	 * Calculates with randomization the delay of traversing 
+	 * the first floor of the elevator trip. 
+	 * @param the median measure to calculate the delay from
+	 * @return a randomized time in seconds of passed measure time to traverse. 
+	 */
+	private double generateDelay(double measureMedian, double measureDeviation) {
+		//Creating value
+		double delay = 0;
+		//Adding the baseline value
+		delay += measureMedian;
+		//Adding random standard deviation 
+		double deviation = measureDeviation *  rng.nextGaussian() *  (int) Math.pow(-1, rng.nextInt(100));
+		delay += deviation;
+		return delay;
+	}
+	
+	
+	
+	
+}
